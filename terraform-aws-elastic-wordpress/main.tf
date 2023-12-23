3c93aca009aa3bf911d97d5d6ed3d989bb57a5e6
LemuelThomas <lemuel.thomas3@gmail.com>
2023-12-23 14:43:35 -0500

    Changed configuration

--- terraform-aws-elastic-wordpress/main.tf
+++ terraform-aws-elastic-wordpress/main.tf
@@ -7,264 +7,88 @@ terraform {
     }
 }
 
-# Create a Class A IPV4 VPC
-resource "aws_vpc" "vpcmain" {
-  cidr_block = "10.16.0.0/16"
-  tags = {
-    Name = "ProjectVPC"
+# Create LT
+resource "aws_launch_template" "project-lt" {
+  name = "project-lt"
+  image_id = "ami-079db87dc4c10ac91"
+  instance_type = "t2.micro"
+  iam_instance_profile {
+    arn = "arn:aws:iam::231442145948:instance-profile/A4LVPC-WordpressInstanceProfile-Cb6jUKO3bYMW"
   }
-}
-
-# Create all the subnets
-resource "aws_subnet" "sn-pub" {
-  vpc_id = aws_vpc.vpcmain.id
-  count = 3
-  availability_zone = var.azs[count.index]
-  cidr_block = var.pub_cidr_blocks[count.index]
-  tags = {
-    Name = var.sn_pub_names[count.index]
+  credit_specification {
+    cpu_credits = "unlimited"
   }
-}
+  tag_specifications {
+    resource_type = "instance"
 
-resource "aws_subnet" "sn-db" {
-  vpc_id = aws_vpc.vpcmain.id
-  count = 3
-  availability_zone = var.azs[count.index]
-  cidr_block = var.db_cidr_blocks[count.index]
-  tags = {
-    Name = var.sn_db_names[count.index]
-  }
-}
-resource "aws_subnet" "sn-app" {
-  vpc_id = aws_vpc.vpcmain.id
-  count = 3
-  availability_zone = var.azs[count.index]
-  cidr_block = var.app_cidr_blocks[count.index]
-  tags = {
-    Name = var.sn_app_names[count.index]
-  }
-}
-
-# Create IGW
-resource "aws_internet_gateway" "igwmain" {
-  vpc_id = aws_vpc.vpcmain.id
-
-  tags = {
-    Name = "project-igw"
-  }
-}
-
-
-# Create RT
-resource "aws_route_table" "rtmain" {
-  vpc_id = aws_vpc.vpcmain.id
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id = aws_internet_gateway.igwmain.id
-  }
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igwmain.id
-  }
-  route {
-    cidr_block = "2600:1f18:3c6c:5900::/56"
-    gateway_id = "local"
-  }
-
-  tags = {
-    Name = "ProjectRT"
-  }
-}
-resource "aws_route_table_association" "rt-pub-a" {
-  subnet_id = aws_subnet.sn-pub[0].id
-  route_table_id = aws_route_table.rtmain.id
-}
-resource "aws_route_table_association" "rt-pub-b" {
-  subnet_id = aws_subnet.sn-pub[1].id
-  route_table_id = aws_route_table.rtmain.id
-}
-resource "aws_route_table_association" "rt-pub-c" {
-  subnet_id = aws_subnet.sn-pub[2].id
-  route_table_id = aws_route_table.rtmain.id
-}
-resource "aws_main_route_table_association" "rt-main-association" {
-  vpc_id = aws_vpc.vpcmain.id
-  route_table_id = aws_route_table.rtmain.id
-}
-
-# Create NACL
-resource "aws_network_acl" "nacl-main" {
-  vpc_id = aws_vpc.vpcmain.id
-  
-  ingress = [
-    {
-      rule_no     = 100
-      action      = "allow"
-      protocol    = -1
-      cidr_block  = "0.0.0.0/0"
-      from_port   = 0
-      to_port     = 0
-      icmp_code = null
-      icmp_type = null
-      ipv6_cidr_block = null
-    },
-    {
-      rule_no     = 101
-      action      = "allow"
-      cidr_block  = null
-      protocol    = -1
-      from_port   = 0
-      to_port     = 0
-      icmp_code = null
-      icmp_type = null
-      ipv6_cidr_block = "::/0"
+    tags = {
+      Name = "Main LT"
     }
-  ]
+  }
+  user_data = filebase64("${path.module}/user-data.sh")
 
-  egress = [
-    {
-      rule_no     = 100
-      action      = "allow"
-      protocol    = -1
-      cidr_block  = "0.0.0.0/0"
-      from_port   = 0
-      to_port     = 0
-      icmp_code = null
-      icmp_type = null
-      ipv6_cidr_block = null
-    },
-    {
-      rule_no     = 101
-      action      = "allow"
-      cidr_block  = null
-      protocol    = -1
-      from_port   = 0
-      to_port     = 0
-      icmp_code = null
-      icmp_type = null
-      ipv6_cidr_block = "::/0"
-    }
-  ]
-  
-  tags = {
-    Name = "ProjectNACL"
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = ["sg-04c80163e1f478289"]
   }
 }
 
-resource "aws_network_acl_association" "naclassocation-pub" {
-  network_acl_id = aws_network_acl.nacl-main.id
-  count = 3
-  subnet_id = aws_subnet.sn-pub[count.index].id
+# Create SSM Parameters
+resource "aws_ssm_parameter" "ssm-db-user" {
+  name        = "/A4L/Wordpress/DBUser"
+  description = "Wordpress Database User"
+  type        = "String"
+  data_type   = "text"
+  tier        = "Standard"
+  value       = "a4lwordpressuser"
 }
-resource "aws_network_acl_association" "naclassocation-app" {
-  network_acl_id = aws_network_acl.nacl-main.id
-  count = 3
-  subnet_id = aws_subnet.sn-app[count.index].id
+resource "aws_ssm_parameter" "ssm-db-name" {
+  name        = "/A4L/Wordpress/DBName"
+  description = "Wordpress Database Name"
+  type        = "String"
+  data_type   = "text"
+  tier        = "Standard"
+  value       = "a4lwordpressdb"
 }
-resource "aws_network_acl_association" "naclassocation-db" {
-  network_acl_id = aws_network_acl.nacl-main.id
-  count = 3
-  subnet_id = aws_subnet.sn-db[count.index].id
+resource "aws_ssm_parameter" "ssm-db-endpoint" {
+  name        = "/A4L/Wordpress/DBEndpoint"
+  description = "WordPress DB Endpoint Name"
+  type        = "String"
+  data_type   = "text"
+  tier        = "Standard"
+  value       = "localhost"
 }
-
-resource "aws_security_group" "wordpress-sg" {
-  name        = "allow-access"
-  description = "Control access to Wordpress Instance(s)"
-  vpc_id      = aws_vpc.vpcmain.id
-
-  ingress {
-    description      = "Allow HTTP IPv4 IN"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.vpcmain.cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Wordpress-SG"
-  }
+resource "aws_ssm_parameter" "ssm-db-password" {
+  name        = "/A4L/Wordpress/DBPassword"
+  description = "Wordpress DB Password"
+  type        = "SecureString"
+  data_type   = "text"
+  tier        = "Standard"
+  value       = "4n1m4l54L1f3"
+  key_id      = "alias/aws/ssm"
 }
-resource "aws_security_group" "database-sg" {
-  name        = "allow-access"
-  description = "Control access to Database"
-  vpc_id      = aws_vpc.vpcmain.id
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Database-SG"
-  }
+resource "aws_ssm_parameter" "ssm-db-root-password" {
+  name        = "/A4L/Wordpress/DBRootPassword"
+  description = "Wordpress DBRoot Password"
+  type        = "SecureString"
+  data_type   = "text"
+  tier        = "Standard"
+  value       = "4n1m4l54L1f3"
+  key_id      = "alias/aws/ssm"
 }
-resource "aws_security_group_rule" "database-sg-rule" {
-  type              = "ingress"
-  description       = "Allow MySQL IN"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.vpcmain.cidr_block]
-  security_group_id = aws_security_group.wordpress-sg.id
+resource "aws_ssm_parameter" "ssm-alb-dns-name" {
+  name        = "/A4L/Wordpress/ALBDNSNAME"
+  description = "DNS Name of the Application Load Balancer for wordpress"
+  type        = "String"
+  data_type   = "text"
+  tier        = "Standard"
+  value       = aws_alb.project-alb.dns_name
 }
-resource "aws_security_group" "efs-sg" {
-  name        = "allow-access"
-  description = "Control access to EFS"
-  vpc_id      = aws_vpc.vpcmain.id
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  
-  tags = {
-    Name = "EFS-SG"
-  }
-}
-resource "aws_security_group_rule" "efs-sg-rule" {
-  type              = "ingress"
-  description       = "Allow NFS/EFS IPv4 IN"
-  from_port         = 2049
-  to_port           = 2049
-  protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.vpcmain.cidr_block]
-  security_group_id = aws_security_group.wordpress-sg.id
-}
-resource "aws_security_group" "loadbalancer-sg" {
-  name        = "allow-access"
-  description = "Control access to Load Balancer"
-  vpc_id      = aws_vpc.vpcmain.id
-
-  ingress {
-    description      = "Allow HTTP IPv4 IN"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.vpcmain.cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "loadbalancer-SG"
-  }
+resource "aws_ssm_parameter" "ssm-efs-id" {
+  name        = "/A4L/Wordpress/EFSFSID"
+  description = "File System ID for Wordpress Content (wp-content)"
+  type        = "String"
+  data_type   = "text"
+  tier        = "Standard"
+  value       = aws_efs_file_system.project-efs.id
 }
--- terraform-aws-elastic-wordpress/variables.tf
+++ terraform-aws-elastic-wordpress/variables.tf
@@ -1,21 +1,3 @@
-variable "azs" {
-  type = list
-}
-variable "pub_cidr_blocks" {
-  type = list
-}
-variable "sn_pub_names" {
-  type = list
-}
-variable "db_cidr_blocks" {
-  type = list
-}
-variable "sn_db_names" {
-  type = list
-}
-variable "app_cidr_blocks" {
-  type = list
-}
-variable "sn_app_names" {
+variable "subnet_ids" {
   type = list
 }
